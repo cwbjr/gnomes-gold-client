ad9b5aec4847db97484233f570b899702f9524d4
Charles Ballew <charleswballewjr@gmail.com>
2018-04-29 12:14:59 -0600

    db done :interrobang:

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -81,7 +81,6 @@ export default {
       title: 'Gnome\'s Gold',
       // dbURL: 'https://gnomes-gold.herokuapp.com/users/',
       dbURL: 'http://localhost:3000/user/',
-      user: {},
       first_name: '',
       last_name: '',
       email: '',
@@ -109,9 +108,9 @@ export default {
       })
         .then(res => res.json())
         .then(() => {
-          this.first_name: '',
-          this.last_name: '',
-          this.email: ''
+          this.first_name = ''
+          this.last_name = ''
+          this.email = ''
         })
     }
   }
